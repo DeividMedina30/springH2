d2b202cc888c27775c504df1d8c2686a20435105
DeividMedina30 <59977494+DeividMedina30@users.noreply.github.com>
2023-03-02 14:56:17 -0500

    first commit

---
+++ spring-H2/src/test/java/com/springH2/springH2/SpringH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springH2.springH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
